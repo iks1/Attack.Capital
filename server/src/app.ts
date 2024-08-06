8b54744c10f4c4810d02ad66896eb3881c30acde
iks1 <rajinertia9239111@gmail.com>
2024-08-06 18:24:53 +0530

    added cors and solved port conflict

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import postRoutes from './routes/postRoutes';
+import cors from 'cors';
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Authorization, Content-Type',
+  };
+  
 
 const app=express();
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 
 
 // const app = express();
-const PORT = 3000;
+const PORT = 3001;
 // const JWT_SECRET = 'somesecret';
 
 // app.use(express.json());
